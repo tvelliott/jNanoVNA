63564f4bd746fe3fcefbd21ec191af81a71e74f7
radioactive <radioactive@debian7>
2019-09-03 17:46:35 -0700

    add license header to source files

--- src/magPanel.java
+++ src/magPanel.java
@@ -1,4 +1,28 @@
 
+/*
+MIT License
+
+Copyright (c) 2019 tvelliott
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
 
 import java.awt.*;
 import java.awt.geom.*;
@@ -233,9 +257,6 @@ public class magPanel extends JPanel
 
     for(int i=0; i<len; i++) {
       double mag = mags[i];
-
-      //slope = (ah - al) / (mh-ml);
-      //intercept = ((ah+al)/2.0) - (slope * ((mh+ml)/2.0));
 
       double slope = (yt-yb) / (0.0-modmin);
       double intercept = ((yt+yb)/2.0) - (slope * ((0.0+modmin)/2.0));
